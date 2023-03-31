8f548d31dce83fa130923a51141e1a96cc6332a0
Eduard Martin Graells <eduard.margra@gmail.com>
2023-03-31 16:07:04 +0200

    Partially refactor grey algorithm
    It doesn't work! :(

--- grey_algorithm.py
+++ grey_algorithm.py
@@ -1,26 +1,41 @@
+""" Remember, to use a file in sage, you have to
+`attach([file])`. 
+
+The grey algorithm simply cycle to all possible edge
+orientations permutations to generate all the possible
+orientations.
+ """
+
 from time import process_time_ns
 
 
-def compute_orientations_grey_algorithm(graph, digirth=0):
+def compute_orientations_grey_algorithm(graph):
     a = process_time_ns()
-    nOrientations = grey_algorithm(graph, digirth=digirth)
+    orientations = grey_algorithm(graph)
     b = process_time_ns()
     print(
-        f"Found {nOrientations} of {graph.tutte_polynomial()(2,0)} acyclic orientations in {(b-a)/1000000000}s"
+        f"Found {orientations} orientations in {(b-a)/1000000000}s"
     )
 
-def grey_algorithm(graph, digirth):
-    """ TODO: you can make this algorithm recursive."""
-    digraph = DiGraph()
-    for edge in graph.edges():
-        u, v, _ = edge
-        digraph.add_edge(u, v)
-    nOrientations = 0
-    for edge in graph.edges():
-        print(digraph.girth())
-        if digraph.girth() >= digirth:
-            nOrientations += 1
-        u, v, _ = edge
-        digraph.delete_edge(u, v)
-        digraph.add_edge(v, u)
-    return nOrientations
+def grey_algorithm(graph):
+    orientations = []
+    initial_orientation = graph.edges(labels=False)
+    orientations.append(initial_orientation)
+    for idx, edge in enumerate(initial_orientation):
+        u, v = edge
+        prev_orientation = orientations[-1]
+        next_orientation = prev_orientation[:idx+1] + [(v,u)] + prev_orientation[idx+2:]
+        import pdb
+        pdb.set_trace()
+        orientations.append(next_orientation)
+    return orientations
+
+
+triangle_orientations = [[(0,1),(1,2),(2,0)]]
+class TestGreyAlgorithm:
+    """How can you test sage files?"""
+
+    def test_grey_algorithm_triangle(self):
+        graph = Graph([(0,1),(1,2),(2,0)])
+        orientations = grey_algorithm(graph)
+        assert orientations == triangle_orientations
