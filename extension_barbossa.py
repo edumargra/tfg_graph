78b518e4aeb619f07ed82776bcb5e573103e142a
Eduard Martin Graells <eduard.margra@gmail.com>
2023-07-16 23:20:22 +0200

    Refactor _compute_orientations_recursive
    A minimal refactor tho

--- extension_barbossa.py
+++ extension_barbossa.py
@@ -52,29 +52,37 @@ def legal_assignments(digraph, w, partialAssig, index, legalAssig, digirth):
         legal_assignments(digraph, w, partialAssig, index + 1, legalAssig, digirth)
 
 
-def compute_orientations(graph, orientations, index=0, digraph=DiGraph(), digirth=0):
-    if index == (graph.order()):
+def _getAlreadyAddedNeighbors(graph, digraph, node_to_add):
+    return list(set(graph.neighbors(node_to_add)).intersection(digraph.vertices()))
+
+def _compute_orientations_recursive(graph, orientations, id_node_to_add, digraph, digirth):
+    if id_node_to_add == graph.order():
         orientations.append(digraph.edges(labels=False))
         return
-    neighbors = list(set(graph.neighbors(index)).intersection(digraph.vertices()))
-    if neighbors == []:
+    neighbors = _getAlreadyAddedNeighbors(graph, digraph, id_node_to_add)
+    if not neighbors:
         newDiGraph = digraph.copy()
-        newDiGraph.add_vertex(index)
-        acyclic_orientations(graph, orientations, index + 1, newDiGraph, digirth)
+        newDiGraph.add_vertex(id_node_to_add)
+        _compute_orientations_recursive(graph, orientations, id_node_to_add + 1, newDiGraph, digirth)
     else:
-        w = neighbors
         legalAssig = []
-        legal_assignments(digraph, w, "0" * len(w), 0, legalAssig, digirth)
+        legal_assignments(digraph, neighbors, "0" * len(neighbors), 0, legalAssig, digirth)
         for assignment in legalAssig:
-            newDiGraph = extend(digraph, w, assignment, index)
-            acyclic_orientations(graph, orientations, index + 1, newDiGraph, digirth)
-    return
+            newDiGraph = extend(digraph, neighbors, assignment, id_node_to_add)
+            _compute_orientations_recursive(graph, orientations, id_node_to_add + 1, newDiGraph, digirth)
+
+
+def compute_orientations(graph, digirth=Infinity):
+    orientations = []
+    digraph = DiGraph()
+    _compute_orientations_recursive(graph, orientations, 0,
+                                           digraph, digirth)
+    return orientations
 
 
 def compute_orientations_with_time(graph, digirth=Infinity):
     a = process_time_ns()
-    orientations = []
-    compute_orientations(graph, orientations, digirth=digirth)
+    orientations = compute_orientations(graph, digirth)
     b = process_time_ns()
     print(
         f"Found {len(orientations)} of {graph.tutte_polynomial()(2,0)} n{digirth} acyclic orientations in {(b-a)/1000000000}s"
